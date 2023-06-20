09debb40ca67e1a0a2e8225161641709de255c3e
504 <504@DESKTOP-OM2NF7O>
2023-06-20 17:22:42 +0900

    day10_연산과제

--- frontend/public/과제/개인과제/과제9_js연산.js
+++ frontend/public/과제/개인과제/과제9_js연산.js
@@ -1,12 +1,22 @@
 //문제11
-let 정수1 = prompt('정수1을 입력하세요')
-let 정수2 = prompt('정수2을 입력하세요')
-let 정수3 = prompt('정수3을 입력하세요')
+let 정수1 = Number(prompt('정수1을 입력하세요'))
+let 정수2 = Number(prompt('정수2을 입력하세요'))
+let 정수3 = Number(prompt('정수3을 입력하세요'))
 let max = 0;
 let mid = 0;
 let min = 0;
 
-정수1 > 정수2 && 정수1 > 정수3 ? max=정수1 : 정수2 > 정수3 ? max=정수2 : max-정수3 > 0 ? max=max : max=정수3 // max
+
+정수1 > 정수2 && 정수1 > 정수3 ? max=정수1 : 
+		정수2 > 정수1 && 정수2 > 정수3 ? max=정수2 : 
+				정수3 > max ? max=정수3 : max=max; //max
+
+정수1 < 정수2 && 정수1 < 정수3 ? min=정수1 :
+		정수2 < 정수1 && 정수2 < 정수3 ? min=정수2 :
+				정수3 < min ? min=정수3 : min=min //min
+
+
+
 정수1 < 정수2 && 정수1 <정수3 ? min=정수1 : 정수2 < 정수3 ? min=정수2 : 정수1 > 정수3 ? min=정수3 : min=정수1 // min
 max > 정수1 && 정수1>min ? mid=정수1 : 
 		max > 정수2 && 정수2>min ? mid=정수2 :
@@ -16,9 +26,10 @@ console.log('최대값'+ max +'중간값' + mid + '최소값' + min)
 
 
 
+
 //문제12
-let player1 = prompt('플레이어1의 값 0, 1, 2 중 입력하세요')
-let	player2 = prompt('플레이어2의 값 0, 1, 2 중 입력하세요')
+let player1 = Number(prompt('플레이어1의 값 0, 1, 2 중 입력하세요'))
+let	player2 = Number(prompt('플레이어2의 값 0, 1, 2 중 입력하세요'))
 
 
 player1 == player2 ? console.log('무승부입니다') : 
