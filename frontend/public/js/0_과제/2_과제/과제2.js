9036a66e9fb6eaa5150f4a82db24c7ff94a0323e
502 <502@DESKTOP-1KV387E>
2023-06-21 20:53:46 +0900

    day11_js과제2(회원가입/로그인창)

--- frontend/public/js/0_과제/2_과제/과제2.js
+++ frontend/public/js/0_과제/2_과제/과제2.js
@@ -1,33 +1,45 @@
 
 
-let inputId = document.querySelector(".input_id").value // 아이디 값
-let inputPw = document.querySelector(".input_pw").value // 비번 값
-let arrayId = []; // 빈배열 만들기
 
-/*
+let arrayId = []; // 빈배열 만들기
+let arrayPw = [];
+
 function 회원가입() { //회원가입 함수
-	
-	if ( inputId.value != '') { // 1. 아이디가 공백이아니면
-		if (inputPw.value != '') { 	// 1-1. 비번이 공백이 아니면
-			console.log(arrayId.push(inputId));
-			alert('회원가입이 완료되었습니다');
-		} else { 	//1-2. 아이디가 공백이 아니지만 비번이 공백이면
-			alert('비밀번호를 입력하지 않았습니다')
-		}
-	} else	// 2. 아이디가 공백이면
-		{alert('아이디를 입력하지 않았습니다')}
-		
-		
+	let inputId = document.querySelector('.input_id').value // 아이디 값
+	let inputPw = document.querySelector('.input_pw').value // 비번 값
+
+	console.log(inputId);
+
+	if (inputId == '') {
+		alert('아이디를 다시 입력하세요');
+	} else if (inputPw == '') {
+		alert('비밀번호를 다시 입력하세요');
+	} else if (inputId.length < 8 || inputPw.length < 8) {
+		alert('아이디와 비밀번호는 8글자 이상이어야합니다');
+	} else if (inputId != '' && inputPw != '') {
+		arrayId.push(inputId);
+		arrayPw.push(inputPw);
+		alert('회원가입이 완료되었습니다');
+
+		document.querySelector('.input_id').value = '';
+		document.querySelector('.input_pw').value = '';}
 	}//회원가입함수end
 
-*/
+	function 로그인() { //로그인 함수
+		let loginId = document.querySelector('.login_id').value // 아이디 값
+		let loginPw = document.querySelector('.login_pw').value // 비번 값
 
+		if (arrayId.indexOf(loginId) == -1) {
+			alert('없는 아이디입니다');
+		} else if (arrayPw.indexOf(loginPw) == -1) {
+			alert('비밀번호가 틀렸습니다');
+		} else {
+			alert('로그인 성공');
 
-function 회원가입() { //회원가입 함수
-	console.log(arrayId.push(inputId));
-	
-	if (inputId == '김') {
-		console.log('하이');
-	} else { console.log('다름'); }
-}//회원가입함수end
+			document.querySelector('.login_id').value = '';
+			document.querySelector('.login_pw').value = '';
+
+		}
+	} //로그인 함수 end
+
 
