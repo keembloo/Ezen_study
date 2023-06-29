387eee5ab534fbdb2614093b386993c543bf8f9e
504 <504@DESKTOP-G6EJM93>
2023-06-29 13:02:29 +0900

    day17_과제4(같이한거)

--- frontend/public/js/0_과제/4_과제(같이한거)/4_과제(같이한거).js
+++ frontend/public/js/0_과제/4_과제(같이한거)/4_과제(같이한거).js
@@ -15,11 +15,23 @@ function onWrite(){
 	let titleValue = document.querySelector('#title').value; 
 	let contentValue = document.querySelector('#content').value; 
 
-
+		// 날짜 양식으로 출력
+		let now = new Date(); // 1. 현재 시스템(pc)의 날짜/시간 호출
+		let year = now.getFullYear(); // 현재연도
+		let month = now.getMonth(); // 현재월 [0 : 1월  / 1 : 2월]
+		let week = now.getDay(); 	// 현재 일 [ 0:일요일 1:월요일 2:화요일 ~ 6:토요일 ]
+		let day = now.getDate();	//  
+		let hour = now.getHours();	// 현재 시
+		let minute = now.getMinutes(); //현재 분
+		let second = now.getSeconds(); //현재 초
+		
+		
 	// 2. 게시물1개당 = 객체1개 선언/ 만들기 = 각 input으로 입력받은 값들을 각 속성명별로 대입해서 객체 생성
 	let board = { writer : writerValue , password : passwordValue ,
 					 title : titleValue , content : contentValue ,
-					 date : new Date()  , view : 0}
+					 date : `${now.getFullYear()}년 ${now.getMonth()+1}월 ${now.getDay()}일
+					 		${now.getHours()} : ${now.getMinutes()} : ${now.getSeconds()}` , 
+					 view : 0 }
 					 
 	 console.log(board); //확인용
 	 
@@ -53,7 +65,7 @@ function onPrint(){ console.log('onPrint()함수실행');
 	//배열내 순차적으로 하나씩 열어보자
 	for( let i = 0; i<boardlist.length; i++) {
 		
-		let board = boardlist[i]; // i번재 배열 인덱스의 객체 호출
+		let board = boardlist[i]; // i번째 배열 인덱스의 객체 호출
 		html += `<tr> 
 					<td> ${i+1} </td>
 					<td onclick="onView(${i})"> ${board.title}</td>
@@ -75,7 +87,7 @@ function onView(index) {console.log('뷰함수실행'+index);
 	let html = `<div>제목 : ${boardlist[index].title}</div>
 				<div>내용 : ${boardlist[index].content} </div>
 				<div>작성자 : ${boardlist[index].writer}</div>
-				<button>삭제</button>
+				<button onclick="onDelete(${index})">삭제</button>
 				<button>수정</button>`;
 	
 	//3. 대입
@@ -89,7 +101,31 @@ function onView(index) {console.log('뷰함수실행'+index);
 } // onView 함수 e
 
 
-
+// 4. 삭제함수
+function onDelete(index){ console.log('삭제함수실행'+index);
+	
+	// 1. 배열내 삭제할 인덱스의 객체 호출
+	let board = boardlist[index];
+	
+	// 2.prompt()함수로 입력받은 값을 password 변수에 저장
+	let password = prompt('비밀번호 : ');
+	
+	// 3. 비교 [삭제할 객체내 비밀번호와 입력받은 비밀번호와 일치하면 삭제 성공]
+	if(board.password == password){
+		// 4. 배열내 해당 객체 삭제
+		boardlist.splice(index , 1); // 삭제할 인덱스번호부터 1개 삭제
+		// 5. 화면 새로고침
+		onPrint(); // 전체 게시물 출력 화면 새로고침
+		
+		document.querySelector('#viewbox').innerHTML = ``;
+		 // 현재 개별 게시물 출력 화면 없애기
+		
+		alert('[삭제성공] 게시물이 삭제되었습니다.');
+		
+	} else { //일치하지 않으면
+		alert('[삭제실패] 비밀번호가 일치하지 않습니다.');
+	}
+} // onDelte 함수 e
 
 
 
