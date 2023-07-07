c3281722d8f017333774af524acf3112fe8920a1
504 <504@DESKTOP-G6EJM93>
2023-07-07 10:22:42 +0900

    day23_게시물상세페이지

--- frontendProject/src/main/webapp/board/view.jsp
+++ frontendProject/src/main/webapp/board/view.jsp
@@ -3,10 +3,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
+	<meta charset="UTF-8">
+	<title>Insert title here</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-
+	<%@include file="../header.jsp" %>
+	<div class="container"> <!-- 부트스트랩 구역 -->
+		
+	</div>
+	<%@include file="../footer.jsp" %>
+	<script src="../js/view.js" type="text/javascript"></script>
 </body>
 </html>
--- frontendProject/src/main/webapp/js/list.js
+++ frontendProject/src/main/webapp/js/list.js
@@ -19,10 +19,50 @@ function boardPrint(){
 		for(let i =0; i<boardList.length; i++){
 			let board = boardList[i]; // i번째 게시물 호출
 			html += `<tr>
-						<td>${board.no}</td><td>${board.title}</td><td>${board.writer}</td>
+						<td>${board.no}</td>
+						<td onclick="onViewLoad(${board.no})">${board.title}</td>
+						<td>${board.writer}</td>
 						<td>${board.date}</td><td>${board.view}</td><td>${board.like}</td>
 					</tr>`;
 		} //for end
 	// 3.대입
 	tcontent.innerHTML = html;
 }  // f end
+
+
+//2. 상세 페이지로 이동 [실행조건 : 클릭한 게시물 제목]
+function onViewLoad( no ){ // 인수 : 클릭한 제목(게시물)의 번호
+	console.log('현재 클릭된 제목(게시물)의 번호 : ' + no);
+	// 클릭된 결과를 다른페이지로 옮기는 방법
+		// JS는 페이지가 전환/새로고침 초기화 =>	// 세션/쿠키 
+	sessionStorage.setItem('no' , no); // 클릭된 게시물 번호 세션에 저장
+	// 2. 페이지 이동
+	location.href="view.jsp"
+	
+} // f end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
