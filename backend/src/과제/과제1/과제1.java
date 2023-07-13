b2f4d444e2b6dd7915493d2c55f504c8c4d79d81
504 <504@DESKTOP-G6EJM93>
2023-07-13 17:44:31 +0900

    0713_JAVA과제1

---
+++ backend/src/과제/과제1/과제1.java
@@ -0,0 +1,28 @@
+package 과제.과제1;
+
+public class 과제1 { // class s
+	public static void main(String[] args) { // main s
+		
+		// 문제1 : 4번
+		// 문제2 : 3번, 4번, 5번, 6번
+		// 문제3 : byte , short, char, int , long
+									// float, double 
+				//boolean
+		
+		
+		// 문제4 : 타입 : int , double
+				// 변수이름 : age , price
+				// 리터럴 : 10, 3.14
+		// 문제5: 3번
+		// 문제6: 2번
+		// 문제7: 4번
+		// 문제8: 3번
+		// 문제9: 1번
+		// 문제10: 2번
+		// 문제11: 13번줄 , 16번줄
+		
+	} // main e
+} // class e
+
+// 1.(2인1조) 연산자 암기 확인
+// 2. (1인) p.73~ p.76 풀이 => 카톡 git 주소 올리기
