67ac0eea7c1dbb960537ac7e33fa7fa9453f6108
504 <504@DESKTOP-G6EJM93>
2023-07-18 13:32:52 +0900

    230718_break, continue

--- backend/src/java1/day04/Ex1_반복문.java
+++ backend/src/java1/day04/Ex1_반복문.java
@@ -90,6 +90,38 @@ public class Ex1_반복문 { // class s
 		} // while e
 		// 위치1
 		
+		//[p.135]
+			//1. for문에 식별이름 정의 : 	for이름 : for() {}
+		
+			//2. break; 
+				// : 가장 가까운 반복문 탈출, continue for이름
+			//2. break for이름;
+				//: 해당 for 탈출, continue for이름
+			//3.continue;
+				//: 가장 가까운 반복문 for증감식/while조건식으로 이동;
+			//3.continue for이름;
+				// : 해당 for 반복문 for증감식/while조건식으로 이동;
+			// 공통점 : break; continue; 아래 코드는 실행x
+		
+		대문자for : for (char upper = 'A'; upper <= 'Z'; upper++) {
+			System.out.println(upper+" ");
+			소문자for : for(char lower ='a'; lower <='z'; lower++) {
+				System.out.println("\t"+lower);
+				// * 가장 가까운 반복문 탈출
+				// if(lower =='g') break;
+				// * 특정 for break;
+				if(lower =='g') break 대문자for;
+			} // for 2 end
+			System.out.println();
+		} // for 1 end
+		
+		// [p.137]
+		숫자for : for (int j=1; j<=10; j++) {
+			if(j%2 !=0) { // 홀수 찾기 ==1
+				continue 숫자for; // for로 이동 // 아래에 있는 코드는 실행x // 홀수 출력x
+			} // if end
+			System.out.println(j+ " "); // 짝수만 출력
+ 		} 
 	} // main e
 } // class e
 
