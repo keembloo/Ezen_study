04cabe4f2772ded9cb06c22dfa194ea367ea6692
504 <504@DESKTOP-G6EJM93>
2023-07-25 12:07:41 +0900

    230725_class 로그인

--- backend/src/과제/과제8/MemberSys.java
+++ backend/src/과제/과제8/MemberSys.java
@@ -51,7 +51,26 @@ public class MemberSys {
 			}// if end
 			else if (ch == 2) {
 				System.out.println("------------- 로그인 -------------");
-			}
+				// 1. 입력받기
+				System.out.println("아이디 : "); String inputId = sc.next();
+				System.out.println("비밀번호 : "); String inputPw = sc.next();
+				
+				// 2. 입력받은 데이터를 배열내 찾기
+				int login = -1; // 로그인 여부를 저장하는 변수 : 못찾았다 0 이상이면 찾고 찾은 인덱스번호 저장 = 로그인성공 기록
+				for ( int i =0; i<memberList.length; i++) {
+					// 만약에 i번째 객체의 id필드와 입력받은 아이디와 같고 i번째 객체의 pw필드와 입력받은 비밀번호와 같으면
+					if( memberList[i] != null && // 비어있는 인덱스는 검사할 필요가 없다!! / i번째 인덱스가 비어있지 않고
+							memberList[i].id.equals(inputId) && memberList[i].password.equals(inputPw)) {
+						login = i; // 로그인 성공후 변수에 성공한 인덱스 저장 = 추후에 로그인 상태 활용
+						break;
+					} 
+				} // for end
+				
+				// 3. 로그인 성공여부 변수에 따른 흐름 제어
+				// 로그인 실패 [ 모든 배열 검사후에 성공 못하면 실패 ]
+				if(login >=0) {System.out.println("안내) 로그인성공");} // 추후에 로그인시 새로운 메뉴 메소드호출
+				else {System.out.println("안내) 로그인실패");}
+			} // if end
 			
 			
 		} // while e
@@ -82,6 +101,10 @@ public class MemberSys {
 				3. 객체를 배열 저장
 				
 			2. 로그인
+				1. 아이디, 비밀번호 입력받아 저장
+				2. 입력받은 값과 배열내 동일한 값 찾기
+					- 만약에 동일한값이 있으면 로긍니 성공
+					- 아니면 실패
 		
 		
 	회원가입,로그인
