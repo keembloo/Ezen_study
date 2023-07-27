bd69033f5b2df001c59e92c2c00d12987b048aeb
504 <504@DESKTOP-G6EJM93>
2023-07-27 11:30:42 +0900

    230727 정적/인스턴드 멤버 이용한 회원제시스템

--- backend/src/과제/과제10/Member.java
+++ backend/src/과제/과제10/Member.java
@@ -1,18 +1,34 @@
 package 과제.과제10;
 
 public class Member {
-	// 1.필드
+	// 1.필드 : 객체의 데이터를 저장하는 곳 = 인스턴스 vs 정적멤버static
 	String id;
 	String password;
 	String name;
 	String phone;
 	int age;
 	
-	// 2.생성자
+	// 2.생성자 : 객체의 초기화를 담당 역할 = 인스턴스 vs 정적멤버static
+		// 1. 빈생성자
+	public Member() { }
+		// 2. 모든 필드를 저장하는 풀생성자
+	//클래스내 빈공간을 오른쪽 클릭 -> Source -> Generate Constructor using Fields -> 모든필드 체크후 생성
 	public Member(String id , String password , String name , String phone, int age) { // 1. 매개변수 5개 정의한 생성자
 		this.id = id; this.password = password; this.name = name; this.phone = phone; this.age = age;
 	}
+		// 3. 특정 개수 필드를 저장하는 생성자 -> 추후에 필요하면 선언
 	
-	// 3.메소드
+	// 3.메소드 : 객체의 행동을 담당 역할 = 인스턴스 vs 정적멤버static
+	
+		// 1. setter or getter
+	
+		// 2. toString : 객체호출시 객체의 주소가 아닌 필드의 값을 반환해주는 함수 [ * 개발자가 객체 상태 확인용 ]
+	//클래스내 빈공간을 오른쪽 클릭 -> Source -> Generate String -> 모든필드 체크후 생성
+	
+	@Override
+	public String toString() { // 객체의 주소값 대신 필드 문자형식으로 반환
+		return "Member [id=" + id + ", password=" + password + ", name=" + name + ", phone=" + phone + ", age=" + age
+				+ "]";
+	}
 	
 }
--- backend/src/과제/과제10/MemberSys.java
+++ backend/src/과제/과제10/MemberSys.java
@@ -7,26 +7,41 @@ import java.util.Scanner;
 import 과제.과제10.Member;
 
 public class MemberSys {
-	static Scanner sc = new Scanner(System.in);
-	static Member[] memberList = new Member[100];
+	static Scanner sc = new Scanner(System.in); // 2. 모든 함수에서 사용 가능한 인스턴스 멤버
+	// 프로그램내 전역구역에서 공유 사용되는 메모리 = 정적멤버
+	// Member[] 객체배열 = new Member[100];				// 1. 인스턴스멤버		=힙영역		=new마다 할당	=> n개
+	static Member[] memberList = new Member[100];	// 2. 정적멤버		=메소드영역	= 프로그램 전체 1개
+	static int login = -1;		// 3. 정적멤버 = 메소드영역 = 로그인을 성공한 인덱스(회원위치) 기억
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) { // main 함수에 static
 		
 		while(true) {
+			// 확인용
+			System.out.println("회원리스트 상태 : "+ Arrays.toString(memberList));
 			
 			System.out.println("\n\n--------- 회원 시스템 ---------"); 
 			System.out.print("1.회원가입 2.로그인 3.아이디찾기 4.비밀번호찾기    선택> ");
+			// 입력객체의 사용 구역 범위 생각!!!! [ 프로그램내 전역 = static ] 
+			// 입력객체는 현재 { } 사용할건지 ... 모든 구역에서 사용할건지.. 다른 클래스에서도, 프로그램내 모든구역
+			// Scanner sc = new Scanner(System.in); // 1.해당 main{ } 안에서만 사용되는 객체
 			int ch = sc.nextInt(); 
 			
+			//if( ch == 1 ) { MemberSys sys = new MemberSys(); sys.회원가입 (); } // main함수 밖에 있는 다른 함수 호출
+			// 1. 인스턴스멤버(static없는) 함수는 객체를 생성하고 호출한다.
+			// 2. 정적멤버(static있는) 함수는 객체없이 클래스를 통한 호출한다.
 			if( ch == 1 ) { 회원가입 (); }
 			else if( ch == 2 ) { 로그인 (); } 
 			else if( ch == 3 ) { 아이디찾기 (); } 
 			else if( ch == 4 ) { 비밀번호찾기 (); } 
 			
 		} // w e 
-	}
+	} // main e
 	
-	static void 회원가입 () {
+	// 1. 회원가입 함수 : 입력받아 저장 [Create]
+	//void 회원가입 () { // 인스턴트 멤버 = 객체 생성해서 할당후 사용
+	static void 회원가입 () { 
+		// 1. 입력받기 -> 각각 5개 입력받기 -> 묶음
+		System.out.print("--------------회원가입--------------");
 		System.out.print("아이디 : ");
 		String id = sc.next();
 		System.out.print("비밀번호 : ");
@@ -38,37 +53,45 @@ public class MemberSys {
 		System.out.print("나이 : ");
 		int age = sc.nextInt();
 
-		
-		Member member = new Member(id, pw, name, phone, age);
+		// 2. 5개 변수를 하나의 객체로 묶음	
+			// 2. 2안
+		Member member = new Member(id, pw, name, phone, age); // member 변수에는 힙주소 저장
+		// 3. 회원의 1개의 객체를 배열 저장
+			// 1. 배열내 빈공간=null을 찾는다
 		for (int i=0; i<memberList.length; i++) {
 			if (memberList[i]==null) {
 				memberList[i]=member; 
 				// System.out.println(memberList[i].id); 
+				System.out.println("회원가입 성공");
 				break;
 			}
 		} // for end
 	} // 회원가입 e
 	
-	static void 로그인 () { 
-		int login = 0;
+	// 2. 로그인 함수 :입력받아 기존에 데이터와 일치하는지 비교 [Reading]
+	static void 로그인 () { // 정적멤버 = 우선할당
 		sc.nextLine();
+		System.out.print("--------------로그인--------------");
+		// 1. 입력받기 -> 각각 2개 입력받기
 		System.out.println("아이디 : ");
 		String inputId = sc.nextLine();
 		System.out.println("비밀번호 : ");
 		String inputPw = sc.nextLine();
+		// 2. 입력받은 값이 배열내 존재하는지 찾기
 		for (int i=0; i<memberList.length; i++) {
 			if (memberList[i] != null && memberList[i].id.equals(inputId) &&  memberList[i].password.equals(inputPw)) {
-				login = 1;
+				login = i; // 정적 멤버 변수에 로그인 성공한 인덱스를 넣어두기 -> 추후에 사용할 목적
 				break;
 			} // if end
 		} // for end
-		if (login >0) {
+		if (login >=0) {
 			System.out.println("로그인 성공");
 		} else {
 			System.out.println("로그인 실패 아이디와 비밀번호를 확인하세요");
 		}
 	} // 로그인 e
 	
+	// 3. 아이디찾기 함수
 	static void 아이디찾기 () {
 		int find = 0;
 		sc.nextLine();
@@ -90,6 +113,7 @@ public class MemberSys {
 		}
 	} // 아이디 찾기 e
 	
+	// 4. 비밀번호찾기 함수
 	static void 비밀번호찾기 () {
 		int find = 0;
 		sc.nextLine();
@@ -156,6 +180,11 @@ public class MemberSys {
 				2. 입력받은 값과 배열내 동일한 값 찾기
 					- 만약에 동일한 값 있으면 *임시 비밀번호(문자4자리 난수생성 ) 업데이트후 출력 
 					- 아니면 없다.
+					
+								// 1. 1안
+		// Member m = new Member(); //빈 생성자
+			// 1. member = { id = null , pw = null , name = null,  phone = null , age =0 }
+		// m.id = id; m.pw = pw; m.name = name; m.phone = phone; m.age = age;   
 */
 
 
