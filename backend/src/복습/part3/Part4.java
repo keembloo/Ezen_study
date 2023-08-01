a04427951f439c5648bfe943265c42dd58dee65a
504 <504@DESKTOP-FRGIVND>
2023-08-01 18:00:18 +0900

    파일처리

--- backend/src/복습/part3/Part4.java
+++ backend/src/복습/part3/Part4.java
@@ -2,47 +2,91 @@ package 복습.part3;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class Part4 {
 	public static void main(String[] args) throws IOException {
-		 Scanner scanner = new Scanner(System.in); 
-		 
-		 FileInputStream fileInputStream = new FileInputStream("./src/java1/day07/Ex1_테스트.txt");
-		 File file = new File("./src/java1/day07/Ex1_테스트.txt");
-		 
-		 byte[] inArray = new byte[(int)file.length()]; 
-		 fileInputStream.read(inArray); 
-		 String str = new String(inArray);
-		 
-		while(true) {
-			System.out.println(" -1: 재고등록  0:결제  "+str);
-			int ch = scanner.nextInt();
-			if ( ch == -1) {
-				System.out.print("제품명 : ");
-				String ch2 = scanner.nextLine();
-				System.out.print("초기재고 : ");
-				String ch3 = scanner.nextLine();
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			String path = "./src/복습/part4/제품정보.txt";
+			FileOutputStream fileOutputStream = new FileOutputStream(path);
+			FileInputStream fileInputStream = new FileInputStream(path);
+			
+			File file = new File(path);
+			byte[] text = new byte[(int)file.length()];
+			fileInputStream.read(text);
+			
+			String 품목 = new String(text);
+			String[] 재고관리 = 품목.split("\n");
+			System.out.println("-1:제품등록 0:결제 1:제품선택");
+			System.out.print("선택>");
+			int ch = sc.nextInt();
+			
+			if(ch==-1) {
+				System.out.print("제품이름 : "); 
+				String 제품명 = sc.next();
+				System.out.print("재고 : ");
+				int 재고 = sc.nextInt();
 				System.out.print("가격 : ");
-				String ch4 = scanner.nextLine();
-				System.out.println("등록되었습니다");
+				int 가격 = sc.nextInt();
+				int 바구니 = 0;
 				
-				
-				 
-			} else if ( ch == 0 ) {
-
-				 String instr = scanner.nextLine();
-				 FileOutputStream fileOutputStream = new FileOutputStream("./src/java1/day07/Ex1_테스트.txt" , true); 
-				 byte[] outArray = instr.getBytes(); 
-				 fileOutputStream.write(outArray);
-			}
-			
-
-			
-			
-		} // while e
-		
+				String str = 제품명+","+재고+","+바구니+","+가격+"\n";
+				fileOutputStream.write(str.getBytes());
+				System.out.println("등록완료");
+			} else if (ch==0) {
+				System.out.println("제품명\t수량\t가격");
+				for(int i=0; i< 재고관리.length;i++) {
+					String 제품 = 재고관리[i].split(",")[0];
+					int 바구니 = Integer.parseInt(재고관리[i].split(",")[2]);
+	    			int 가격 = Integer.parseInt(재고관리[i].split(",")[3]);
+	    			if(바구니 > 0) {
+	    				System.out.println(제품 + "\t" + 바구니 + "\t" + (바구니*가격));
+	    			}
+	    		} // for e
+	    	}else if(ch == 1) {
+	    		for(int i = 0; i < 재고관리.length; i++) {
+	    			String 제품 = 재고관리[i].split(",")[0];
+	    			int 재고 = Integer.parseInt(재고관리[i].split(",")[1]);
+	    			if(재고 > 0) {
+	    				System.out.println((i+1) + ". " + 제품 + "[" + 재고 + "]");
+	    			}else{
+	    				System.out.println((i+1) + ". " + 제품 + "[재고없음]");
+	    			}
+	    		}// for e
+	    		System.out.println("선택 >>> ");
+	    		ch = sc.nextInt();
+	    		for(int i = 0; i < 재고관리.length; i++) {
+	    			if(ch == (i+1)) {
+		    			String 제품 = 재고관리[i].split(",")[0];
+		    			int 재고 = Integer.parseInt(재고관리[i].split(",")[1]);
+		    			int 바구니 = Integer.parseInt(재고관리[i].split(",")[2]);
+		    			int 가격 = Integer.parseInt(재고관리[i].split(",")[3]);
+	    				if(재고 > 0) {
+	    					재고--;
+	    					바구니++;
+	    					재고관리[i] = 제품 + "," + 재고 + "," + 바구니 + "," + 가격;
+	    					
+	    					String str = "";
+	    					for(int j = 0; j < 재고관리.length; j++) {
+	    						str += 	재고관리[j].split(",")[0] + "," +
+	    								재고관리[j].split(",")[1] + "," +
+	    								재고관리[j].split(",")[2] + "," +
+	    								재고관리[j].split(",")[3] + "," + "\n";
+	    					}
+	    					FileOutputStream fileOutputStream2 = new FileOutputStream(path);
+	    					fileOutputStream2.write(str.getBytes());
+	    					System.out.println(제품 + "를 담았습니다.");
+	    					break;
+	    				}else {
+	    					System.out.println("재고가 없습니다.");
+	    				}
+	    			} // if end
+	    		} // for end
+	    	}//if end
+		}// while e
 	} // main e
 } // class e
--- backend/src/복습/part4/Part4.java
+++ backend/src/복습/part4/Part4.java
@@ -10,43 +10,91 @@ import java.util.Scanner;
 
 public class Part4 {
 	public static void main(String[] args) throws IOException {
-		Scanner scanner = new Scanner(System.in);
-		while (true) {
-			String path = "./src/java1/day07/Ex1_테스트.txt";
-			FileOutputStream fileOutputStream = new FileOutputStream(path , true);
-			FileInputStream fileInputStream = new FileInputStream(path);
+		Scanner sc = new Scanner(System.in);
+		while(true) {
 			
-			File file = new File(path);
-			byte[] inArray = new byte[(int)file.length()];
-			fileInputStream.read(inArray);
-			String str = new String(inArray);
-			String[] 재고관리 = str.split("\n");
+		    //String filePath = "./src/복습/part4/제품정보.txt";
+	
+		    FileOutputStream fileOutputStream = new FileOutputStream( filePath ,true); 
+	
+		    FileInputStream fileInputStream = new FileInputStream( filePath ); 
+	
+		    File file = new File( filePath );
 			
-			System.out.println("-1:제품등록 0:결제 "+str);
-			System.out.println("선택>>");
-			int ch = scanner.nextInt();
-			
-			if( ch == -1) {
-				scanner.nextLine();
-				System.out.print("제품명 : ");
-				String 제품 = scanner.nextLine();
-				System.out.print("초기재고 : ");
-				int 재고 = scanner.nextInt();
-				System.out.print("가격 : ");
-				int 가격 = scanner.nextInt();
-				System.out.println("등록되었습니다");
-				int 바구니 = 0;
-				
-				String addstr = 제품+","+재고+","+가격+","+바구니;
-				fileOutputStream.write(addstr.getBytes());
-				System.out.println("등록완료");
-				
-			} else if ( ch ==0) {
-				
-			}else if (ch >0) {
-				
-			}
-			
-		}// while e
+		    byte[] textInfo = new byte[(int)file.length()];
+		    fileInputStream.read(textInfo);
+		    
+		    String products = new String(textInfo);
+		    String[] 재고관리 = products.split("\n");
+	    
+	    	System.out.println("---------------메뉴-------------");
+	    	System.out.println("-1:제품등록 0:결제 1:제품선택");
+	    	int ch = sc.nextInt();
+	    	
+	    	if(ch == -1) {
+	    		System.out.print("제품이름 >>> "); String Pname = sc.next();
+	    		System.out.print("초기재고 >>> "); int 재고 = sc.nextInt();
+	    		System.out.print("제품가격 >>> "); int price = sc.nextInt();
+	    		int basket = 0;
+	    		
+	    		String outStr = Pname +"," + 재고 + "," + basket + "," + price + "\n";
+	    		fileOutputStream.write(outStr.getBytes());
+	    		System.out.println("제품등록 완료");
+	    	}	// if end
+	    	
+	    	else if(ch == 0) {
+	    		System.out.println("제품명\t수량\t가격");
+	    		for(int i = 0; i < 재고관리.length; i++) {
+	    			String name = 재고관리[i].split(",")[0];
+	    			int 바구니 = Integer.parseInt(재고관리[i].split(",")[2]);
+	    			int 가격 = Integer.parseInt(재고관리[i].split(",")[3]);
+	    			if(바구니 > 0) {
+	    				System.out.println(name + "\t" + 바구니 + "\t" + (바구니*가격));
+	    			}
+	    		}
+	    		
+	    	}else if(ch == 1) {
+	    		for(int i = 0; i < 재고관리.length; i++) {
+	    			String name = 재고관리[i].split(",")[0];
+	    			int 재고 = Integer.parseInt(재고관리[i].split(",")[1]);
+	    			if(재고 > 0) {
+	    				System.out.println((i+1) + ". " + name + "[" + 재고 + "]");
+	    			}else{
+	    				System.out.println((i+1) + ". " + name + "[재고없음]");
+	    			}
+	    		}
+	    		
+	    		System.out.println("선택 >>> ");
+	    		ch = sc.nextInt();
+	    		for(int i = 0; i < 재고관리.length; i++) {
+	    			if(ch == (i+1)) {
+		    			String name = 재고관리[i].split(",")[0];
+		    			int 재고 = Integer.parseInt(재고관리[i].split(",")[1]);
+		    			int 바구니 = Integer.parseInt(재고관리[i].split(",")[2]);
+		    			int 가격 = Integer.parseInt(재고관리[i].split(",")[3]);
+	    				if(재고 > 0) {
+	    					재고--;
+	    					바구니++;
+	    					재고관리[i] = name + "," + 재고 + "," + 바구니 + "," + 가격;
+	    					
+	    					String outStr = "";
+	    					for(int j = 0; j < 재고관리.length; j++) {
+	    						outStr += 	재고관리[j].split(",")[0] + "," +
+	    									재고관리[j].split(",")[1] + "," +
+	    									재고관리[j].split(",")[2] + "," +
+	    									재고관리[j].split(",")[3] + "," + "\n";
+	    					}
+	    					FileOutputStream fileOutputStream2 = new FileOutputStream(filePath);
+	    					fileOutputStream2.write(outStr.getBytes());
+	    					System.out.println(name + "를 담았습니다.");
+	    					break;
+	    				}else {
+	    					System.out.println("재고가 없습니다.");
+	    				}
+	    			} // if end
+	    		} // for end
+	    		
+	    	} //else if end
+	    } // while end
 	} // main e
 } // class e
