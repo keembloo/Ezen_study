5e33452d622f2c7334459aeb6764142cbfacee50
504 <504@DESKTOP-FRGIVND>
2023-08-03 14:48:57 +0900

    230803_다형성

---
+++ backend/src/java1/day16/Ex2_매개변수의다형성/Ex2_실행.java
@@ -0,0 +1,26 @@
+package java1.day16.Ex2_매개변수의다형성;
+
+public class Ex2_실행 {
+	public static void main(String[] args) {
+		
+		// 0.
+		Driver driver = new Driver();
+		
+		// 1. 부모객체
+		Vehicle vehicle = new Vehicle();
+			System.out.println("-----> 1.차량대입");
+			driver.drive(3, vehicle); // 객체의 타입[Vehicle]과 매개변수 타입[Vehicle]일치 가능
+			
+		// 2. 자식객체
+		Bus bus = new Bus();
+			System.out.println("-----> 1.버스대입");
+			driver.drive(5, bus);  // 객체의 타입[Bus] 매개변수 타입 [Vehicle]	타입이 불일치인데 가능
+		// 2. 자식객체
+		Taxi taxi = new Taxi();
+			System.out.println("-----> 1.택시대입");
+			//driver.drive("안녕",taxi); // 매개변수의 타입 불일치라 오류 
+		driver.drive(3,taxi);	// 매개변수의 타입 불일치라 오류 [ 단 부모-자식 관계일때 가능 ]
+		
+		// 
+	}
+}
