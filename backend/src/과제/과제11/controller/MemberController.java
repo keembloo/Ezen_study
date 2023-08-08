2dca500295cad14a2e5ee796ad3726a9835517b3
504 <504@DESKTOP-FRGIVND>
2023-08-08 11:25:49 +0900

    230808_아이디찾기,비밀번호찾기

--- backend/src/과제/과제11/controller/MemberController.java
+++ backend/src/과제/과제11/controller/MemberController.java
@@ -34,5 +34,29 @@ public class MemberController {
 		boolean result = MemberDao.getInstance().loginSQL(id , pw);
 		return result;
 	} //loginLogic end
+	
+	
+	// 4.
+	public String findById(String name, String phone) {
+		// - 테스트. view 전달받은 매개변수 확인
+		System.out.println("view value :" + name + phone);
+		
+		// Dao에게 매개변수 전달하고 찾은 아이디 반환 받기
+		String result = MemberDao.getInstance().findById(name , phone);
+		return result;
+		// if (result !=) {핸드폰문자전송함수}
+		//return ; //실패
+	}
+	
+	// 5.
+	public String findByPw(String id , String phone) {
+		// - 테스트. view 전달받은 매개변수 확인
+		System.out.println("view value :" + id + phone);
+		
+		// Dao에게 매개변수 전달하고 찾은 아이디 반환 받기
+		return MemberDao.getInstance().findByPw(id , phone);
+		
+		//return null; //실패
+	}
 } // class e
 
--- backend/src/과제/과제11/view/MainPage.java
+++ backend/src/과제/과제11/view/MainPage.java
@@ -16,12 +16,15 @@ public class MainPage {
 	public void mainView() {
 		while(true) {
 			System.out.println("\n\n ========= 회원제 커뮤니티 ==========");
-			System.out.println("1.회원가입 2.로그인 선택 : ");
+			System.out.println("1.회원가입 2.로그인 선택 3.아이디찾기 4.비밀번호찾기 선택 : ");
 			
 			try {
 				int ch = sc.nextInt(); // 만약에 타입과 일치하지 않는 값을 입력하면 InputMismatchException
 				if(ch ==1) { signupView();}
 				if(ch==2) {loginView();}
+				if(ch==3) {findById();}
+				if(ch==4) {findByPw();}
+				
 			}catch ( Exception e) {
 				System.out.println("경고) 잘못 입력했습니다.");
 				sc = new Scanner(System.in); // 입력객체의 메모리를 새롭게 만들어주기 
@@ -57,5 +60,32 @@ public class MainPage {
 				MemberController.getInstance().loginLogic(id , pw);
 		if (result) {System.out.println("안내) 로그인 성공");}
 		else {System.out.println("경고) 로그인실패");}
+	}
+	
+	// 4. 아이디 찾기 
+	public void findById() {
+		System.out.println("------ 아이디찾기 페이지 ------");
+		System.out.print("이름 : ");
+		String name = sc.next();
+		System.out.print("전화번호 : ");
+		String phone = sc.next();
+		
+		String result = MemberController.getInstance().findById(name, phone);
+		
+		if (result !=null) {System.out.println("회원님의 아이디 : "+result);}
+		else {System.out.println("경고) 정보가 일치하는 아이디가 없습니다.");}
+	}
+	
+	// 5. 비밀번호 찾기
+	public void findByPw() {
+		System.out.println("------ 비밀번호찾기 페이지 ------");
+		System.out.print("아이디 : ");
+		String id = sc.next();
+		System.out.print("전화번호 : ");
+		String phone = sc.next();
+		
+		String result = MemberController.getInstance().findByPw(id, phone);
+		if (result !=null) {System.out.println("회원님의 비밀번호 : "+result);}
+		else {System.out.println("경고) 정보가 일치하는 아이디가 없습니다.");}
 	}
 }
