c66ba53cc8af4e4160b96b201898de3e472b09ce
keembloo <guesstty@naver.com>
2023-08-13 14:47:20 +0900

    복습

--- backend/src/복습/part7/controller/MemberController.java
+++ backend/src/복습/part7/controller/MemberController.java
@@ -1,7 +1,28 @@
 package 복습.part7.controller;
 
-import java1.day08.Ex5.Member;
+import 복습.part7.model.dao.MemberDao;
+import 복습.part7.model.dto.MemberDto;
+import 복습.part7.view.MainPage;
 
 public class MemberController {
+	private static MemberController memberController = new MemberController();
+	public static MemberController getInstance() {return memberController;}
+	private MemberController() {}
+	
 
+	
+	public boolean signup(String id , String pw , String name , String phone , int age) {
+		MemberDto member = new MemberDto(id, pw, name, phone, age);
+		for (int i =0; i<MemberDao.memberList.length; i++) {
+			if (MemberDao.memberList[i]==null) {
+				MemberDao.memberList[i]= member;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void login() {
+		
+	}
 }
--- backend/src/복습/part7/view/MainPage.java
+++ backend/src/복습/part7/view/MainPage.java
@@ -2,6 +2,45 @@ package 복습.part7.view;
 
 import java.util.Scanner;
 
-public class MainPage {
+import 복습.part7.controller.MemberController;
 
+public class MainPage {
+	private static MainPage mainPage = new MainPage();
+	public static MainPage getInstance() {return mainPage;}
+	private MainPage() {}
+	
+	private Scanner sc = new Scanner(System.in);
+	
+	// 1. 메인메뉴
+	public void main() {
+		while(true) {
+			System.out.print("1.회원가입 2.로그인 선택 :");
+			
+			try {
+				int ch =sc.nextInt();
+				if(ch==1) {signup();}
+				if(ch==2) {login();}
+				
+			} catch (Exception e) {
+				System.out.println(e);
+				sc = new Scanner(System.in);
+			}
+		}
+	}
+	
+	public void signup() {
+		System.out.println("회원가입");
+		System.out.print("아이디 : "); String id = sc.next();
+		System.out.print("비밀번호 : "); String pw = sc.next();
+		System.out.print("이름 : "); String name = sc.next();
+		System.out.print("전화번호 : "); String phone = sc.next();
+		System.out.print("전화번호 : "); int age = sc.nextInt();
+		
+		boolean result = MemberController.getInstance().signup(id , pw , name , phone , age);
+	
+	}
+	
+	public void login() {
+		
+	}
 }
