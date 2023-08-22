43a777ba33f6497042a0ebe2f4f2efe93ef8d66f
504 <504@DESKTOP-FRGIVND>
2023-08-22 14:01:27 +0900

    230822_jsp 방문록

---
+++ jspweb/src/main/webapp/visitlog/visitlog.js
@@ -0,0 +1,28 @@
+console.log('js열림');
+
+// 1. Create(저장)
+function vwrite(){
+	
+	// 1. HTML input 태그 호출
+	// 2. 객체화
+	// 3. 유효성검사
+	// 4. AJAX 
+	
+		// 5. 결과에 따른 코드
+	
+} // f e
+
+
+// 2. Read(호출)
+function vread(){
+	
+} // f e 
+// 3. Update(수정)
+function vupdate(){
+	
+} // f e
+
+// 4. Delete(삭제)
+function vdelete(){
+	
+} // f e
