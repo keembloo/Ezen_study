14513a54230352b418416dcfbd06adb8cd070b16
504 <504@DESKTOP-GR47Q4P>
2023-09-21 12:28:39 +0900

    230921_포인트함수

--- jspweb/src/main/java/model/dao/MemberDao.java
+++ jspweb/src/main/java/model/dao/MemberDao.java
@@ -1,8 +1,11 @@
 package model.dao;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.dto.MemberDto;
+import model.dto.MpointDto;
 
 public class MemberDao extends Dao{
 	// 싱글톤 객체
@@ -104,6 +107,63 @@ public class MemberDao extends Dao{
 			if (count==1) {return true;} // 삭제성공 -> 회원탈퇴
 		}catch (Exception e) { System.out.println(e); }
 		return false; // 회원번호 또는 입력받은 패스워드 일치하지 않거나
+	}
+	
+	
+	
+	// 9. 포인트 지급/사용에 대한 함수
+	public boolean setPoint( MpointDto dto ) {
+		try {
+			String sql = "insert into mpoint(mpno , mno , mpamount , mpcomment ) values(?, ? , ? , ?)";
+			ps = conn.prepareStatement(sql);
+			ps.setString(1,dto.getMpno());
+			ps.setInt(2,dto.getMno());
+			ps.setString(3,dto.getMpcomment());
+			ps.setString(4,dto.getMpcomment());
+			
+			int result = ps.executeUpdate();
+				if (result == 1) {
+					return true;
+				} else {
+					return false;
+				}
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
+		return false;
+	}
+	
+	// 10. 내 포인트 확인 [ 로그인한 사람의 현재 포인트 합계 ]
+	public long getPoint(int mno) {
+		try { 	// sum(필드명) : 총합계를 계산할 필드명 인수로 대입 // avg // count
+			String sql = "select sum(mpamount) from mpoint where mno = ?"; // 합계구하기
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			if ( rs.next()) {
+				return rs.getInt(1);
+			}
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
+		return 0;
+	}
+	
+	// 11. 내 포인트 사용 내역 출력
+	public List<MpointDto> getPointAll(int mno){
+		List<MpointDto> list = new ArrayList<>();
+		try {
+			String sql = "select * from mpoint where mno = ?";
+			ps = conn.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				
+				
+				list.add( );
+			}
+		} catch (Exception e) {
+			System.out.println(e.getStackTrace());
+		}
+		return null;
 	}
 	
 }
